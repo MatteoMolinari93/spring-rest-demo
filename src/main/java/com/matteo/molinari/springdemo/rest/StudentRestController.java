42a161a7ec70663f1e6d9370d7aa11509bfc9a0a
Matteo Molinari <matteomolinari93@gmail.com>
2020-07-01 19:29:30 +0200

    PathVariable

--- src/main/java/com/matteo/molinari/springdemo/rest/StudentRestController.java
+++ src/main/java/com/matteo/molinari/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.matteo.molinari.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +15,26 @@ import com.matteo.molinari.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-
-	@GetMapping("/students")
-	public List<Student> getStudents(){
-		List<Student> students = new ArrayList();
+	
+	private List<Student> students;
+	
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList();
 		
 		students.add(new Student("Andreas", "Nefzger"));
 		students.add(new Student("Warren", "Heffernan"));
 		students.add(new Student("Sebastian", "Pirpidel"));
-		
+	}
+
+	@GetMapping("/students")
+	public List<Student> getStudents(){
 		return students;
+	}
+	
+	@GetMapping("students/{id}")
+	public Student getStudent(@PathVariable int id) {
+		return students.get(id);
 	}
 	
 }
