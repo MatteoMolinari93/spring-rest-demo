39e32721364521efe445e1238a14feeb4df6691e
Matteo Molinari <matteomolinari93@gmail.com>
2020-07-01 20:14:23 +0200

    Cleanup

--- src/main/java/com/matteo/molinari/springdemo/rest/StudentRestController.java
+++ src/main/java/com/matteo/molinari/springdemo/rest/StudentRestController.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +20,7 @@ public class StudentRestController {
 	
 	@PostConstruct
 	public void loadData() {
-		students = new ArrayList();
+		students = new ArrayList<Student>();
 		
 		students.add(new Student("Andreas", "Nefzger"));
 		students.add(new Student("Warren", "Heffernan"));
